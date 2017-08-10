e7d2afaf6bc667b1b97afed9502242ac89ca701d
chapin <997155658@qq.com>
2017-08-10 10:01:00 +0000

    added v2

---
+++ v2/run.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/my-docker/v2/cgroups"
+	"github.com/my-docker/v2/cgroups/subsystems"
+	"github.com/my-docker/v2/container"
+	"os"
+	"strings"
+)
+
+/*func Run(tty bool, command string) {
+	parent := container.NewParentProcess(tty, command)
+	if err := parent.Start(); err != nil {
+		log.Error(err)
+	}
+	parent.Wait()
+	os.Exit(-1)
+}*/
+
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New Parent process error")
+		return
+	}
+
+	if err := parent.Start(); err != nil {
+		log.Error(err)
+	}
+
+	// create a new groupManager
+	cgroupManager := cgroups.NewCgroupManager("mydocker-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
+
+	sendInitCommand(comArray, writePipe)
+
+	parent.Wait()
+
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
+}
