b2bd364fe91d1281ff10cdbaef4d3e948c42a4c1
chapin <997155658@qq.com>
2017-08-16 09:20:41 +0000

    finished v5

---
+++ v5/cgroups/subsystems/subsystem.go
@@ -0,0 +1,24 @@
+package subsystems
+
+// resource limit config
+type ResourceConfig struct {
+	MemoryLimit string
+	CpuShare    string
+	CpuSet      string
+}
+
+//Subsystem
+type Subsystem interface {
+	Name() string
+	Set(path string, res *ResourceConfig) error
+	Apply(path string, pid int) error
+	Remove(path string) error
+}
+
+var (
+	SubsystemsIns = []Subsystem{
+		&CpusetSubSystem{},
+		&MemorySubSystem{},
+		&CpuSubSystem{},
+	}
+)
